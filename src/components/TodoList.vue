5a07beac37034fa15e5aaba64295377d5413b0f1
bjoernkneisel <info@bjoern-kneisel.de>
2020-10-19 09:40:33 +0200

    BEM SCSS

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -40,3 +40,80 @@
     </header>
   </div>
 </template>
+
+<script>
+export default {
+  inject: ["global"],
+  setup() {
+    return {};
+  },
+  methods: {
+    activeTasks() {
+      const activeTasks = this.global.state.tasks.filter((task) => !task.done);
+      const activeLength = activeTasks.length;
+      const taskPluralSingular =
+        activeLength > 1 || activeLength < 1 ? "tasks" : "task";
+      return `${activeLength} active ${taskPluralSingular}`;
+    },
+  },
+  computed: {
+    dateNow() {
+      const d = new Date();
+      return `${d.getDate()} / ${d.getMonth() + 1} / ${d.getFullYear()}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+  flex-wrap: wrap;
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
+  &__date {
+    &-date {
+      font-size: 20px;
+      margin-bottom: 5px;
+    }
+    &-active {
+      font-size: 13px;
+      color: #61dbfb;
+    }
+    @media (max-width: 768px) {
+      &-active {
+        margin-bottom: 15px;
+      }
+    }
+  }
+  &__nav {
+    &-list {
+      display: flex;
+    }
+    &-item {
+      margin: 0 15px;
+      color: #7e8187;
+      cursor: pointer;
+      &--active {
+        color: #d3d4d6;
+      }
+    }
+    @media (max-width: 768px) {
+      &-item {
+        margin-left: 0;
+      }
+    }
+    @media (max-width: 700px) {
+      &-list {
+        flex-direction: column;
+      }
+      &-item {
+        margin-bottom: 12px;
+      }
+    }
+  }
+}
+</style>
