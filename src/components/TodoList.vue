ba4b26165fced24f23d93381bdfc0f7748b7b32b
bjoernkneisel <info@bjoern-kneisel.de>
2020-10-19 09:39:56 +0200

    TodoList Template

---
+++ src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <header class="header">
+      <div class="header__date">
+        <h1 class="header__date-date">{{ dateNow }}</h1>
+        <h3 class="header__date-active">{{ activeTasks() }}</h3>
+      </div>
+
+      <nav class="header__nav">
+        <ul class="header__nav-list">
+          <li
+            @click="global.filterTodos"
+            class="header__nav-item"
+            :class="{
+              'header__nav-item--active': global.state.activeTab === 'all',
+            }"
+          >
+            All Tasks
+          </li>
+          <li
+            @click="global.filterTodos('undone')"
+            class="header__nav-item"
+            :class="{
+              'header__nav-item--active': global.state.activeTab === 'undone',
+            }"
+          >
+            Incomplete Tasks
+          </li>
+          <li
+            @click="global.filterTodos('done')"
+            class="header__nav-item"
+            :class="{
+              'header__nav-item--active': global.state.activeTab === 'done',
+            }"
+          >
+            Complete Tasks
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
